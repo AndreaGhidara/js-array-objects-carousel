9945c508f535c1c24c4ee8918bb9bc8a7bd86ece
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-05-03 17:46:12 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+const container = document.querySelector(".container");
+const carusel = document.querySelector(".carusel");
+const btnNext = document.querySelector(".next");
+const btnPrev = document.querySelector(".previous");
+
+
+let counter = 0;
+console.log(images[counter].text);
+
+const slider = document.createElement("div");
+slider.classList.add("sliders");
+
+const imageSlider = document.createElement("img");
+imageSlider.src = `./assets/${images[0].image}`; //SRC del immagine
+imageSlider.classList.add("absolute", "w-100");//aggingi classe
+
+const titleSlider = document.createElement("h3");
+titleSlider.innerText = images[0].title//Titolo della slider
+titleSlider.classList.add("title");
+
+const textSlider = document.createElement("p");
+textSlider.innerText = images[0].text//testo della slider
+textSlider.classList.add("text");
+
+carusel.append(slider);
+slider.append(imageSlider);
+slider.append(titleSlider);
+slider.append(textSlider);
+
+btnNext.addEventListener("click", () => {
+    if (counter == images.length - 1) {
+        counter = 0;
+        imageSlider.src = `./assets/${images[counter].image}`; //SRC del immagine
+        titleSlider.innerText = images[counter].title//Titolo della slider
+        textSlider.innerText = images[counter].text//testo della slider
+    } else {
+        counter++;
+
+        imageSlider.src = `./assets/${images[counter].image}`; //SRC del immagine
+        titleSlider.innerText = images[counter].title//Titolo della slider
+        textSlider.innerText = images[counter].text//testo della slider
+
+    }
+
+})
+
+btnPrev.addEventListener("click", () => {
+    if (counter == 0) {
+        counter = images.length - 1;
+        imageSlider.src = `./assets/${images[counter].image}`; //SRC del immagine
+        titleSlider.innerText = images[counter].title//Titolo della slider
+        textSlider.innerText = images[counter].text//testo della slider
+    } else {
+        counter--;
+        imageSlider.src = `./assets/${images[counter].image}`; //SRC del immagine
+        titleSlider.innerText = images[counter].title//Titolo della slider
+        textSlider.innerText = images[counter].text//testo della slider
+
+    }
+
+})
